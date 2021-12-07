a9670312b66fbf63b519d6147e138758f2155b44
Hämälistö Heikki <heikki.hamalisto@gmail.com>
2021-12-08 01:00:46 +0200

    Day 7

---
+++ 2021/07/AdventOfCode2021Day7/AdventOfCode2021Day7/main.swift
@@ -0,0 +1,71 @@
+
+import Foundation
+
+func readCrabData(fromFile file: String) -> [Int] {
+    let currentDirectoryURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+    let bundleURL = URL(fileURLWithPath: "Main.bundle", relativeTo: currentDirectoryURL)
+    let bundle = Bundle(url: bundleURL)
+    if let filepath = bundle?.path(forResource: file, ofType: "txt") {
+        do {
+            let content = try String(contentsOfFile: filepath, encoding: .utf8)
+            let lines = content.components(separatedBy: .newlines)
+            guard let firstLine = lines.first else { return [] }
+            
+            return firstLine.split(separator: "," )
+            .compactMap { Int($0) }
+        } catch {
+            print("Contents could not be loaded")
+        }
+    } else {
+        print("\(file).txt not found")
+    }
+    return []
+}
+
+func sumOfFirstNaturalNumbers(n: Int) -> Int {
+    return n * (n+1) / 2
+}
+
+func findOptimalHorizontalMove(forCrabs crabs: [Int], constantFuelRate: Bool = true) -> (move: Int?, fuel: Int?) {
+    guard crabs.isEmpty == false else { return (nil, nil) }
+    let average = crabs.reduce(0, +) / crabs.count
+    let min = crabs.min()!
+    let max = crabs.max()!
+    let minAverageDifference = abs(average - min)
+    let maxAverageDifference = abs(average - max)
+    let sorted = crabs.sorted { (minAverageDifference < maxAverageDifference) ? $0 < $1 : $1 < $0 }
+    
+    var optimumMove: Int?
+    var optimumFuelCost: Int?
+    moveLoop: for move in 1...max {
+        var fuel = 0
+        crabLoop: for crab in sorted {
+            let crabFuel = (constantFuelRate) ? abs(crab - move) : sumOfFirstNaturalNumbers(n: abs(crab - move))
+            fuel += crabFuel
+            if let optimumFuelCost = optimumFuelCost, fuel > optimumFuelCost {
+                break crabLoop
+            }
+        }
+        if let optimumFuelCost_ = optimumFuelCost {
+            if fuel < optimumFuelCost_ {
+                optimumFuelCost = fuel
+                optimumMove = move
+            }
+        } else {
+            optimumMove = move
+            optimumFuelCost = fuel
+        }
+    }
+    return (optimumMove, optimumFuelCost)
+
+}
+
+let crabs = readCrabData(fromFile: "input")
+// PART 1
+print("Optimal move with constant fuel rate: \(findOptimalHorizontalMove(forCrabs: crabs, constantFuelRate: true))")
+
+
+// PART 2
+print("Optimal move with increasing fuel rate: \(findOptimalHorizontalMove(forCrabs: crabs, constantFuelRate: false))")
+
+
